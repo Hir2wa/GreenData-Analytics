a57cd4ba9839ac94c9afd7792d6b0b795ff0d691
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-21 21:13:26 +0200

    Load Raw Dataset

--- notebooks/data_analysis.py
+++ notebooks/data_analysis.py
@@ -2,3 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+df = pd.read_csv("../data/raw/uber.csv")
+
+print("First 5 rows:\n", df.head())
+print("\nInfo:\n", df.info())
+print("\nSummary Stats:\n", df.describe())
