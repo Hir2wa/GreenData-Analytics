bd3606db3bdb2bcfb20aeb35c2aee84777cb8e66
Hir2wa <alainfabricehirwa@gmail.com>
2025-07-21 21:14:04 +0200

    clean dataset

--- notebooks/data_analysis.py
+++ notebooks/data_analysis.py
@@ -7,3 +7,6 @@ df = pd.read_csv("../data/raw/uber.csv")
 print("First 5 rows:\n", df.head())
 print("\nInfo:\n", df.info())
 print("\nSummary Stats:\n", df.describe())
+
+df = df.dropna()
+print("Nulls remaining:\n", df.isnull().sum())
